5fe2e423245c6236e5f35c2643a38b5faad9da99
WambiruL <wambirulorraine@gmail.com>
2021-07-23 21:16:03 +0300

    Add the login page

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,12 +1,9 @@
 from . import views
 from django.conf.urls import url 
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns=[
-   
     url(r'register/',views.registerPage,name='register'),
+    url(r'login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
 ]
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
