a8a96957f7bfaf6a444fa1913e90a406ff4ac95d
WambiruL <wambirulorraine@gmail.com>
2021-07-23 22:45:09 +0300

    Style the navbar

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -7,4 +7,5 @@ urlpatterns=[
     url(r'register/',views.registerPage,name='register'),
     url(r'login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
     url(r'^$',views.index,name = 'index'),
+    url(r'^logout/',auth_views.LogoutView.as_view(),name='logout',),
 ]
